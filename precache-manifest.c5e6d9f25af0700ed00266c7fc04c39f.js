cebea9d3d9fa1cac48c0a3167245b48a1a4ad7cb
Nizwm Borgoyari <borgoyarinizwm@gmail.com>
2022-05-09 04:22:25 +0530

    Updates

---
+++ precache-manifest.c5e6d9f25af0700ed00266c7fc04c39f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "705d7c6a1e3635c882ab",
+    "url": "/travel-journal/static/css/main.da99d7d9.chunk.css"
+  },
+  {
+    "revision": "705d7c6a1e3635c882ab",
+    "url": "/travel-journal/static/js/main.705d7c6a.chunk.js"
+  },
+  {
+    "revision": "d81810a4e64de93cb072",
+    "url": "/travel-journal/static/js/1.d81810a4.chunk.js"
+  },
+  {
+    "revision": "6b67c87f7fe4c66fb3a7",
+    "url": "/travel-journal/static/js/runtime~main.6b67c87f.js"
+  },
+  {
+    "revision": "f17c27ed0e17be48b62823b71c39738b",
+    "url": "/travel-journal/index.html"
+  }
+];
